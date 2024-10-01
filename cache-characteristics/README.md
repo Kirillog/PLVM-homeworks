cadcd1675c75aba810012ed6b208e48694c2fe54
Kirill Mitkin <kirill.mitkin.03@gmail.com>
2024-10-01 23:34:56 +0300

    add block size calc

---
+++ cache-characteristics/README.md
@@ -0,0 +1,21 @@
+# Cache characteristics
+
+### Build
+```sh
+cmake -B build && cmake --build build
+```
+### Run
+```sh
+./build/cache_spec
+```
+### Test
+Tested on AMD Ryzen 5 PRO 2500U:
+- Capacity      32768
+- Associativity 8
+- Block size    64
+
+```
+Capacity: 32768
+Associativity: 8
+Block size: 64
+```
--- cache-characteristics/src/main.cpp
+++ cache-characteristics/src/main.cpp
@@ -13,21 +13,20 @@ alignas(4 * 1024) char mem[MEM_SIZE];
 typedef std::chrono::duration<double, std::nano> duration;
 typedef std::chrono::time_point<std::chrono::high_resolution_clock> time_point;
 
-void set_next_ptr(uint32_t index, char * ptr) {
-	char** addr = reinterpret_cast<char**>(&mem[index]);
+void set_next_ptr(char* loc, char * ptr) {
+	char** addr = reinterpret_cast<char**>(loc);
 	*addr = ptr;
 }
 
 void init_mem(uint32_t stride, uint32_t spots) {
-	set_next_ptr(0, &mem[stride * (spots - 1)]);
+	set_next_ptr(mem, &mem[stride * (spots - 1)]);
 	for (int i = 0; i < spots - 1; ++i) {
-		set_next_ptr(stride * (i + 1), &mem[stride * i]);
+		set_next_ptr(mem + stride * (i + 1), &mem[stride * i]);
 	}
 }
 
 duration time(uint32_t stride, uint32_t spots) {
-	init_mem(stride, spots);
-	char * ptr = mem;
+	char * ptr = &mem[stride * (spots - 1)];
 #define STEP_1 ptr = *reinterpret_cast<char **>(ptr);
 #define STEP_10 STEP_1 STEP_1 STEP_1 STEP_1 STEP_1 STEP_1 STEP_1 STEP_1 STEP_1 STEP_1
 #define STEP_100 STEP_10 STEP_10 STEP_10 STEP_10 STEP_10 STEP_10 STEP_10 STEP_10 STEP_10 STEP_10
@@ -43,10 +42,12 @@ duration time(uint32_t stride, uint32_t spots) {
 }
 
 std::optional<uint32_t> try_estimate_assoc(uint32_t stride) {
+	init_mem(stride, 1);
 	auto prev_time = time(stride, 1);
 	for (uint32_t assoc = 2; assoc < MAX_ASSOC; ++assoc) {
+		init_mem(stride, assoc);
 		auto cur_time = time(stride, assoc);
-		if (cur_time.count() / (double)prev_time.count() > 1.2) {
+		if (cur_time.count() / prev_time.count() > 1.2) {
 			return assoc - 1;
 		}
 		prev_time = cur_time;
@@ -60,13 +61,19 @@ std::pair<uint32_t, uint32_t> try_estimate_size_assoc() {
 	while (stride >= 16) {
 		auto assoc = try_estimate_assoc(stride);
 		if (prev_assoc.has_value() && assoc.has_value() && (*prev_assoc) * 2 == *assoc) {
-			return {stride, *prev_assoc};
+			return {stride * *assoc, *prev_assoc};
 		}
 		prev_assoc = assoc;
 		stride >>= 1;
 	}
 	return {0, 0};
 }
+
+struct CacheSpecs {
+	uint32_t capacity;
+	uint32_t associativity;
+	uint32_t block_size;
+};
 
 std::pair<uint32_t, uint32_t> detect_capacity_associativity() {
 	std::map<std::pair<uint32_t, uint32_t>, uint32_t> count;
@@ -75,7 +82,6 @@ std::pair<uint32_t, uint32_t> detect_capacity_associativity() {
 	}
 	std::pair<uint32_t, uint32_t> key = {0, 0};
 	for (const auto& [k, v] : count) {
-		std::cerr << k.first << " " << k.second << " " << v << "\n";
 		if (v > count[key]) {
 			key = k;
 		}
@@ -83,13 +89,48 @@ std::pair<uint32_t, uint32_t> detect_capacity_associativity() {
 	return key;
 }
 
-std::optional<uint32_t> detect_cache_size() {
+uint32_t try_estimate_block_size(uint32_t cap, uint32_t assoc) {
+	uint32_t stride = cap / assoc;
+	uint32_t bl = assoc / 2;
+  	duration prev_time;
+	for (int cur_block_size = 16; cur_block_size < 2 * 128; cur_block_size <<= 1) {
+		for (int i = 0; i < bl - 1; ++i) {
+			set_next_ptr(mem + stride * (i + 1), &mem[stride * i]);
+		}
+		char *next = mem + stride * bl + cur_block_size;
+		set_next_ptr(next, &mem[stride * (bl - 1)]);
+		for (int i = 0; i < bl; ++i) {
+			set_next_ptr(next + stride * (i + 1), &next[stride * i]);
+		}
+		init_mem(stride, assoc / 2 + 1);
+		set_next_ptr(mem, next + stride * bl);
+		auto cur_time = time(stride, 1);
+		if (prev_time.count() / cur_time.count() > 1.2) {
+			return cur_block_size;
+		}
+		prev_time = cur_time;
+	}
+	return 0;
+} 
 
+uint32_t detect_cache_size(uint32_t cap, uint32_t assoc) {
+	std::map<uint32_t, uint32_t> count;
+	for (int _ = 0; _ < 100; ++_) {
+		++count[try_estimate_block_size(cap, assoc)];
+	}
+	uint32_t key = 0;
+	for (const auto& [k, v] : count) {
+		if (v > count[key]) {
+			key = k;
+		}
+	}
+	return key;
 }
 
 int main() {
 	auto [cap, assoc] = detect_capacity_associativity();
 	std::cout << "Capacity: " << cap << "\n";
 	std::cout << "Associativity: " << assoc << "\n";
+	std::cout << "Block size: " << detect_cache_size(cap, assoc) << "\n";
 	return 0;
 }
